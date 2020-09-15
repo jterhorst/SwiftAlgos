5b1b90f1563936de5c4cb1ec357382952251abaf
Jason Terhorst <yourstruly@jterhorst.com>
2020-09-14 20:52:20 -0500

    Finished friend suggestion algo

--- GraphMutualFriends.playground/Contents.swift
+++ GraphMutualFriends.playground/Contents.swift
@@ -1,14 +1,17 @@
-import UIKit
+import Foundation
 
-/**
- Your manager at FB would like you to create a new algo to create potential connections between users with shared friends in common.
+/*:
+ 
  How would we design this model?
+ 
+ * What data structure would we use to track and identify neighboring vertices?
+ * What traversal or memoization techniques would we use?
+ 
+*/
 
- What data structure would we use to track and identify neighboring vertices?
- What traversal or memoization techniques would we use?
- */
 
 class Connection {
+    
     var target: Person
 
     init(_ target: Person) {
@@ -16,15 +19,35 @@ class Connection {
     }
 }
 
-class Person {
+class Person: Hashable, Equatable {
+    
+    let uuid: UUID = UUID()
+    
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
     var connections: [Connection] = []
     var name: String
+    var hashValue: Int {
+        var hasher = Hasher()
+        hasher.combine(self.uuid)
+        return hasher.finalize()
+    }
 
     init(_ name: String, friends:[Person]) {
         self.name = name
         for buddy in friends {
             connections.append(Connection(buddy))
         }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.uuid)
+    }
+    
+    func addConnection(person: Person) {
+        connections.append(Connection(person))
     }
 }
 
@@ -35,13 +58,44 @@ func findSharedConnections(person: Person) -> [Person] {
         return []
     }
 
+    var allConnectionCounts: [Person:Int] = [:]
+
+    for c in person.connections {
+        for relatedConnection in c.target.connections {
+            if relatedConnection.target != person {
+                allConnectionCounts[relatedConnection.target, default: 0] += 1
+            }
+        }
+    }
     
+    var recommendedConnections:[Person] = []
+    
+    for (person, count) in allConnectionCounts {
+        if count > 1 {
+            recommendedConnections.append(person)
+        }
+    }
 
-    var possibleConnections:[Person] = []
-
-
-
-    return possibleConnections
+    return recommendedConnections
 }
 
+let friendG = Person("G", friends: [])
+let friendF = Person("F", friends: [])
+let friendA = Person("A", friends: [])
+let friendD = Person("D", friends: [])
+let friendC = Person("C", friends: [friendG, friendF, friendD, friendA])
+let friendE = Person("E", friends: [friendC])
+let friendB = Person("B", friends: [friendA, friendD])
+friendA.addConnection(person: friendC)
+friendA.addConnection(person: friendB)
+friendD.addConnection(person: friendC)
+friendD.addConnection(person: friendB)
 
+let suggestions = findSharedConnections(person: friendA)
+assert(suggestions.contains(friendD))
+assert(suggestions.count == 1)
+print("found \(suggestions.count) suggested friends")
+
+for person in suggestions {
+    print("here's another suggested friend!... \(person.name)")
+}
